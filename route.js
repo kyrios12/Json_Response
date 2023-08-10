e12ec38daf5d77b3164002e052bb56a71b71d7a4
Abdul Azeez <103828615+kyrios12@users.noreply.github.com>
2023-08-10 11:51:10 +0530

    Setting up Views

--- route.js
+++ route.js
@@ -3,14 +3,16 @@ const port = process.env.port || 8000;
 
 const application = jsexpress();
 
+application.set('view engine','ejs');
+
+
+application.get('/', (req, res) => {
+    res.render('view');
+  });
 
 
 
 
-
-
-
-
-app.listen(port, () => {
+application.listen(port, () => {
     console.log(`Server is running on port ${port}`);
   });
